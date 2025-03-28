f5fd5973d9cb447d3ccdfb18c9dbaf430f383c11
Astro247 <manuelsarti40@gmail.com>
2025-03-28 12:12:55 +0100

    First SQL Explanation: How to create and delete a database using the CREATE and DROP functions.

---
+++ SQL_create-database.sql
@@ -0,0 +1,8 @@
+
+-- Per creare un database, è sufficiente eseguire la query "CREATE DATABASE" e poi inserire il nome che verrà assegnato al database
+
+CREATE DATABASE db_test;
+
+-- Per cancellare un database basta eseguire la query "DROP DATABASE", dove drop sta per "lascia cadere", e inserire il nome del database.
+
+DROP DATABASE db_test;
